ab2f16d23dc0195ddc087a802ef4990246013175
nightblade9 <nightbladecodes@gmail.com>
2024-11-10 01:11:00 -0500

    Lex engine: look for game.json, with GameName property.
    Added documentation on how to use LexEngine

--- source/LexEngine.Core/Console/LexGame.cs
+++ source/LexEngine.Core/Console/LexGame.cs
@@ -1,11 +1,24 @@
 ﻿namespace LexEngine.Core;
 
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class LexGame
 {
     public void Run()
     {
-        Console.WriteLine("Hello from LexEngine!");
+        var gameJsonPath = Path.Join("Content", "game.json");
+
+        if (!File.Exists(gameJsonPath))
+        {
+            throw new InvalidOperationException("Content/game.json file is missing!");
+        }
+
+        var gameJsonContents = File.ReadAllText(gameJsonPath);
+        var gameJson = JsonConvert.DeserializeObject(gameJsonContents) as JObject;
+        var gameName = gameJson["GameName"];
+
+        Console.WriteLine($"Welcome to {gameName}!");
     }
 }
