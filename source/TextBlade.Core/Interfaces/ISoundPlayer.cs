f5477ffee7ab428b6306532eef4804c13baa8b02
Stephane <stephane.grobety@git.ch>
2024-11-29 10:57:39 +0100

    explosed required methodes

--- source/TextBlade.Core/Interfaces/ISoundPlayer.cs
+++ source/TextBlade.Core/Interfaces/ISoundPlayer.cs
@@ -1,5 +1,16 @@
-﻿namespace TextBlade.Core.Interfaces;
+﻿using System.ComponentModel;
+
+namespace TextBlade.Core.Interfaces;
 
 public interface ISoundPlayer: IDisposable
 {
+    public event AsyncCompletedEventHandler? LoadCompleted;
+    bool IsLoadCompleted { get; }
+    string SoundLocation { get; set; }
+    void Load();
+    void LoadAsync();
+    void Play();
+    void PlayLooping();
+    void PlaySync();
+    void Stop();
 }
