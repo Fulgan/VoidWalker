e68c1a570f728a8c4f881b4f6012c562a7837534
nightblade9 <nightbladecodes@gmail.com>
2024-12-03 23:25:42 -0500

    Refactor: split validation out of logic

--- source/TextBlade.Core/Characters/PartyManagement/EquipmentEquipper.cs
+++ source/TextBlade.Core/Characters/PartyManagement/EquipmentEquipper.cs
@@ -6,6 +6,45 @@ public static class EquipmentEquipper
 {
     internal static IEnumerable<string> EquipIfRequested(Item itemData, Inventory inventory, List<Character> party)
     {
+        Equipment equipment = ValidateArguments(itemData, inventory, party);
+
+        var messages = DisplayEquipmentStats(equipment, party);
+        foreach (var message in messages)
+        {
+            yield return message;
+        }
+
+        yield return "Equip for who? Or pess 0 to cancel.";
+
+        var input = 0;
+        if (!int.TryParse(Console.ReadLine().Trim(), out input))
+        {
+            yield return "Cancelling.";
+            yield break;
+        }
+
+        if (input <= 0 || input > party.Count)
+        {
+            yield return "Invalid number, cancelling.";
+        }
+
+        var partyMember = party[input - 1];
+        var equipped = partyMember.EquippedOn(equipment.ItemType);
+
+        var equipMessage = $"Equipped on {partyMember.Name}";
+        if (equipped != null)
+        {
+            equipMessage = $"{equipMessage}, replacing {equipped.Name}";
+            inventory.Add(equipped);
+        }
+
+        partyMember.Equipment[itemData.ItemType] = equipment;
+        inventory.Remove(equipment.Name);
+        yield return equipMessage;
+    }
+
+    private static Equipment ValidateArguments(Item itemData, Inventory inventory, List<Character> party)
+    {
         if (itemData == null)
         {
             throw new ArgumentException("Item data is missing", nameof(itemData));
@@ -32,39 +71,7 @@ public static class EquipmentEquipper
             throw new InvalidOperationException($"Item data for {itemData.Name} doesn't seem to be Equipment. Is the $type field specified correctly in Items.json?");
         }
 
-        var messages = DisplayEquipmentStats(equipment, party);
-        foreach (var message in messages)
-        {
-            yield return message;
-        }
-
-        yield return "Equip for who? Or pess 0 to cancel.";
-
-        var input = 0;
-        if (!int.TryParse(Console.ReadLine().Trim(), out input))
-        {
-            yield return "Cancelling.";
-            yield break;
-        }
-
-        if (input <= 0 || input > party.Count)
-        {
-            yield return "Invalid number, cancelling.";
-        }
-
-        var partyMember = party[input - 1];
-        var equipped = partyMember.EquippedOn(equipment.ItemType);
-        
-        var equipMessage = $"Equipped on {partyMember.Name}";
-        if (equipped != null)
-        {
-            equipMessage = $"{equipMessage}, replacing {equipped.Name}";
-            inventory.Add(equipped);
-        }
-
-        partyMember.Equipment[itemData.ItemType] = equipment;
-        inventory.Remove(equipment.Name);
-        yield return equipMessage;
+        return equipment;
     }
 
     private static IEnumerable<string> DisplayEquipmentStats(Equipment itemData, List<Character> party)
