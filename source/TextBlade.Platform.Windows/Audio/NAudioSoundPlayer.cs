786262b5be80acfa1c0da884ff6d2705cdfdea5e
nightblade9 <nightbladecodes@gmail.com>
2024-12-09 22:20:53 -0500

    Audio looping works!

--- source/TextBlade.Platform.Windows/Audio/NAudioSoundPlayer.cs
+++ source/TextBlade.Platform.Windows/Audio/NAudioSoundPlayer.cs
@@ -9,21 +9,32 @@ namespace TextBlade.Platform.Windows.Audio;
 public class NAudioSoundPlayer : ISoundPlayer
 {
     internal const string SupportedAudioExtension = "ogg";
+    private const float VolumeMultiplier = 0.7f;
 
     private WaveOutEvent? _waveOut;
     private VorbisWaveReader? _reader;
     
     public void Load(string audioFile)
     {
-        
         _reader = new VorbisWaveReader($"{audioFile}.{SupportedAudioExtension}");
+
         _waveOut = new WaveOutEvent();
         _waveOut.Init(_reader);
+        _waveOut.Volume = VolumeMultiplier;
+
+        // Don't do this for one-off audio. Which we don't have yet.
+        _waveOut.PlaybackStopped += RewindAndPlay;
     }
 
     public void Play() => _waveOut?.Play();
 
     public void Stop() => _waveOut?.Stop();
+
+    private void RewindAndPlay(object? sender, StoppedEventArgs args)
+    {
+        _reader.Seek(0, SeekOrigin.Begin);
+        Play();
+    }
 
     protected virtual void Dispose(bool disposing)
     {
