8d2e9d124786535dce47395deb0e37aa93a91761
nightblade9 <nightbladecodes@gmail.com>
2024-12-17 23:45:27 -0500

    Battle results applier tests
    Battle command *is* a command. Oops. Needed for testing.

--- source/TextBlade.Core/Commands/IBattleCommand.cs
+++ source/TextBlade.Core/Commands/IBattleCommand.cs
@@ -3,7 +3,7 @@ namespace TextBlade.Core.Commands;
 /// <summary>
 /// Used for things that are self-contained battles. Like, "battle engines" or whatever.
 /// </summary>
-public interface IBattleCommand
+public interface IBattleCommand : ICommand
 {
     public bool IsVictory { get; }
     public int TotalGold { get; }
