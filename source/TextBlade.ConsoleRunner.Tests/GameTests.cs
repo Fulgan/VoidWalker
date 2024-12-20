69266df1223b91fdb805d1119af270f496bb2298
nightblade9 <nightbladecodes@gmail.com>
2024-12-19 21:42:40 -0500

    Fix: Game.SaveGame broken test

--- source/TextBlade.ConsoleRunner.Tests/GameTests.cs
+++ source/TextBlade.ConsoleRunner.Tests/GameTests.cs
@@ -42,13 +42,11 @@ public class GameTests
     public void SaveGame_UsesExistingSaveData_IfCurrentLocationHasNone()
     {
         // Arrange
-        // Previous save location was DiceDungeon
+        // Previous save location was a dungeon
         var game = CreateGameStub();
+        var dungeon = new Dungeon("Dinky Dungeon", "It's ... dinky ...", 3, ["Black Mold", "White Mold"], "Mulder") { LocationId = "DinkyDungeon.json"};
+        game.CurrentLocation = dungeon;
         game.SaveGame();
-
-        var saveData = SaveGameManager.LoadGame(SaveGameManager.CurrentGameSlot);
-        var previousSaveData = new Dungeon("Dice Dungeon", "DD", 10, ["Slime"], "Slime-X").GetCustomSaveData();
-        SaveGameManager.SaveGame(SaveGameManager.CurrentGameSlot, saveData.LocationSpecificDataLocationId, new List<Core.Characters.Character>(), new Inventory(), 0, "dicedungeon.json", previousSaveData);
 
         // Current location is an inn
         var inn = new Inn("Snoozeville", "It's made of clouds", "SnoozeVilleInn") { LocationId = "snoozeVille.json" };
@@ -63,7 +61,7 @@ public class GameTests
         var actual = JsonConvert.DeserializeObject<SaveData>(File.ReadAllText(saveFileName));
         Assert.That(actual.LocationSpecificData, Is.Not.Null);
         Assert.That(actual.LocationSpecificData.Keys, Does.Contain("CurrentFloor"));
-        Assert.That(actual.LocationSpecificDataLocationId, Is.EqualTo(inn.LocationId));
+        Assert.That(actual.LocationSpecificDataLocationId, Is.EqualTo(dungeon.LocationId));
     }
 
     public GameStub CreateGameStub()
